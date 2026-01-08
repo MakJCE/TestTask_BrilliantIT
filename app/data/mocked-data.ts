b849ca3908eb763129121dd5938c270987cf4976
Jhonny Camacho E <makykamacho@gmail.com>
2026-01-08 18:42:20 -0300

    add mocked data

---
+++ app/data/mocked-data.ts
@@ -0,0 +1,10 @@
+export const leaderboardData = [
+    { rank: 1, model: 'Model Name1', overall: '0.00', reasoning: '0.00', coding: '0.00', math: '0.00', truthful: '0.00', jailbreak: '0.00', tokens: '0.00', latency: 'LMM-124' },
+    { rank: 2, model: 'Model Name2', overall: '0.00', reasoning: '0.00', coding: '0.00', math: '0.00', truthful: '0.00', jailbreak: '0.00', tokens: '0.00', latency: 'LMM-124' },
+    { rank: 3, model: 'Model Name3', overall: '0.00', reasoning: '0.00', coding: '0.00', math: '0.00', truthful: '0.00', jailbreak: '0.00', tokens: '0.00', latency: 'LMM-124' },
+    { rank: 4, model: 'Model Name4', overall: '0.00', reasoning: '0.00', coding: '0.00', math: '0.00', truthful: '0.00', jailbreak: '0.00', tokens: '0.00', latency: 'LMM-124' },
+    { rank: 5, model: 'Model Name5', overall: '0.00', reasoning: '0.00', coding: '0.00', math: '0.00', truthful: '0.00', jailbreak: '0.00', tokens: '0.00', latency: 'LMM-124' },
+    { rank: 6, model: 'Model Name6', overall: '0.00', reasoning: '0.00', coding: '0.00', math: '0.00', truthful: '0.00', jailbreak: '0.00', tokens: '0.00', latency: 'LMM-124' },
+    { rank: 7, model: 'Model Name7', overall: '0.00', reasoning: '0.00', coding: '0.00', math: '0.00', truthful: '0.00', jailbreak: '0.00', tokens: '0.00', latency: 'LMM-124' },
+    { rank: 8, model: 'Model Name8', overall: '0.00', reasoning: '0.00', coding: '0.00', math: '0.00', truthful: '0.00', jailbreak: '0.00', tokens: '0.00', latency: 'LMM-124' },
+];
